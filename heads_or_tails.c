f319b3b5cb7ca82341b2d54a15021c619acb25ef
Existance494 <dash25@purdue.edu>
2026-06-03 14:32:45 -0400

    Working game files
    Game is in a playable state and no longer crashes, runs perfectly

---
+++ heads_or_tails.c
@@ -0,0 +1,275 @@
+#include "heads_or_tails.h"
+
+#include <conio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <Windows.h> // Sleep(n) will make program sleep for n milliseconds
+
+#ifndef STDIO_H
+#include <stdio.h>
+#endif
+
+enum coins {
+    heads = 1,
+    tails
+};
+
+void normal_mode(FILE *fp) {
+    int score = 0;
+    int highscore = 0;
+    if (fp == NULL) {
+        highscore = 0;
+    }
+    if (fp != NULL) {
+        fscanf(fp, "Highscore: %d", &highscore);
+        printf("\nGood luck!\n");
+    }
+    srand(time(NULL));
+    while(1) {
+        int value = rand() % (2 - 1 + 1) + 1;
+        printf("What side will the coin land on? (Heads or Tails)\n");
+        char guess[6];
+        scanf("%s", guess);
+        for (int i = 0; guess[i] != '\0'; i++) {
+            guess[i] = tolower(guess[i]);
+        }
+        if (value == heads) {
+            if ((strcmp(guess, "heads") == 0) || (strcmp(guess, "head") == 0) || (strcmp(guess, "h") == 0)) {
+                score++;
+                printf("Correct! You have now guessed ");
+                printf("%d", score);
+                printf(" coin flips in a row correctly!\n");
+            }
+            else {
+                printf("Incorrect! You got ");
+                printf("%d", score);
+                printf(" guesses correct in a row, try again next time\n");
+                break;
+            }
+        }
+        else {
+            if ((strcmp(guess, "tails") == 0) || (strcmp(guess, "tail") == 0) || (strcmp(guess, "t") == 0)) {
+                score++;
+                printf("Correct! You have now guessed ");
+                printf("%d", score);
+                printf(" coin flips in a row correctly!\n");
+            }
+            else {
+                printf("Incorrect! You got ");
+                printf("%d", score);
+                printf(" guesses correct in a row, try again next time\n");
+                break;
+            }
+        }
+    }
+    if (score > highscore) {
+        highscore = score;
+        printf("You achieved a new highscore of ");
+        printf("%d", score);
+        printf(" consecutive correct guesses!\n");
+        FILE *fp2 = fopen("highscore.txt", "w");
+        fprintf(fp2, "Highscore: ");
+        fprintf(fp2, "%d\n", highscore);
+        fprintf(fp2, "\n");
+        if (fp2 != NULL) {
+            fclose(fp2);
+        }
+        fp2 = NULL;
+    }
+    else if (score == highscore) {
+        printf("You tied your highscore of ");
+        printf("%d", score);
+        printf(" consecutive correct guesses!\n");
+    }
+    else if (score < highscore) {
+        printf("You couldn't beat your highscore of ");
+        printf("%d", highscore);
+        printf(" consecutive correct guesses...\n");
+        printf("You were ");
+        printf("%d", (highscore - score));
+        printf(" guesses off\n");
+        printf("Better luck next time!\n");
+    }
+    printf("Press any key to move on\n");
+    _getch();
+}
+
+void ten_guess(FILE *fp) {
+    printf("10 guess mode\n");
+    int num_correct;
+    for (int i = 0; i < 10; i++) {
+        int value = rand() % (2 - 1 + 1) + 1;
+        printf("%d. What side will the coin land on? (Heads or Tails)\n", i+1);
+        char guess[6];
+        scanf("%s", guess);
+        for (int i = 0; guess[i] != '\0'; i++) {
+            guess[i] = tolower(guess[i]);
+        }
+        if (value == heads) {
+            if ((strcmp(guess, "heads") == 0) || (strcmp(guess, "head") == 0) || (strcmp(guess, "h") == 0)) {
+                num_correct++;
+                printf("Correct! You have now guessed ");
+                printf("%d", num_correct);
+                printf(" coin flips correctly!\n");
+            }
+            else {
+                printf("Incorrect!\n");
+            }
+        }
+        else {
+            if ((strcmp(guess, "tails") == 0) || (strcmp(guess, "tail") == 0) || (strcmp(guess, "t") == 0)) {
+                num_correct++;
+                printf("Correct! You have now guessed ");
+                printf("%d", num_correct);
+                printf(" coin flips correctly!\n");
+            }
+            else {
+                printf("Incorrect!\n");
+            }
+        }
+    }
+    printf("You have guessed ");
+    printf("%d", num_correct);
+    printf(" coin flips correctly!\n");
+    printf("That is a ");
+    float rate = (num_correct / 10.0) * 100;
+    printf("%d%% success rate!\n", (int)rate );
+    printf("Press any key to move on\n");
+    _getch();
+}
+
+void hundred_guess(FILE *fp) {
+    printf("Welcome to Custom Length Guess Mode\n");
+    printf("How many rounds would you like to guess?\n");
+    int num_correct;
+    int length;
+    scanf("%d", &length);
+    for (int i = 0; i < length; i++) {
+        int value = rand() % (2 - 1 + 1) + 1;
+        printf("%d. What side will the coin land on? (Heads or Tails)\n", i+1);
+        char guess[6];
+        scanf("%s", guess);
+        for (int i = 0; guess[i] != '\0'; i++) {
+            guess[i] = tolower(guess[i]);
+        }
+        if (value == heads) {
+            if ((strcmp(guess, "heads") == 0) || (strcmp(guess, "head") == 0) || (strcmp(guess, "h") == 0)) {
+                num_correct++;
+                printf("Correct! You have now guessed ");
+                printf("%d", num_correct);
+                printf(" coin flips correctly!\n");
+            }
+            else {
+                printf("Incorrect!\n");
+            }
+        }
+        else {
+            if ((strcmp(guess, "tails") == 0) || (strcmp(guess, "tail") == 0) || (strcmp(guess, "t") == 0)) {
+                num_correct++;
+                printf("Correct! You have now guessed ");
+                printf("%d", num_correct);
+                printf(" coin flips correctly!\n");
+            }
+            else {
+                printf("Incorrect!\n");
+            }
+        }
+    }
+    printf("You have guessed ");
+    printf("%d", num_correct);
+    printf(" coin flips correctly!\n");
+    printf("That is a ");
+    float rate = (num_correct / (float)length) * 100;
+    printf("%.2f%% success rate!\n", rate );
+    printf("Press any key to move on\n");
+    _getch();
+}
+
+void check_highscores(FILE *fp) {
+    int highscore = 0;
+    if (fp == NULL) {
+        highscore = 0;
+    }
+    if (fp != NULL) {
+        fscanf(fp, "Highscore: %d", &highscore);
+        printf("Your current highscore is ");
+        printf("%d", highscore);
+        printf("\nGood luck!\n");
+    }
+    printf("Press any key to move on\n");
+    _getch();
+}
+
+void instructions(FILE *fp) {
+    printf("Welcome to the heads/tails game!\n");
+    printf("The game is pretty simple, you have to predict which side a coin is going to land on\n");
+    printf("Press any key to move on\n");
+    _getch();
+}
+
+void main_menu(FILE *fp) {
+    while(1) {
+        printf("___________________________________________________________________________________________\n");
+        printf(" __  __       _         __  __                  \n");
+        printf("|  \\/  | __ _(_)_ __   |  \\/  | ___ _ __  _   _ \n");
+        printf("| |\\/| |/ _` | | '_ \\  | |\\/| |/ _ \\ '_ \\| | | |\n");
+        printf("| |  | | (_| | | | | | | |  | |  __/ | | | |_| |\n");
+        printf("|_|  |_|\\__,_|_|_| |_| |_|  |_|\\___|_| |_|\\__,_|\n");
+        printf("___________________________________________________________________________________________\n");
+        printf("\n");
+        printf("1. Normal Mode\n");
+        printf("2. 10 Guess Challenge\n");
+        printf("3. Custom Length Guess challenge\n");
+        printf("4. Check Highscores\n");
+        printf("5. How to play\n");
+        printf("6. Exit Game\n");
+        printf("\n");
+        printf("Choose a game mode (Type a number 1-6 to choose):\n");
+        char selection[2];
+        scanf("%s", selection);
+        if ((strcmp(selection, "1") == 0)) {
+            normal_mode(fp);
+        }
+        else if ((strcmp(selection, "2") == 0)) {
+            ten_guess(fp);
+        }
+        else if ((strcmp(selection, "3") == 0)) {
+            hundred_guess(fp);
+        }
+        else if ((strcmp(selection, "4") == 0)) {
+            check_highscores(fp);
+        }
+        else if ((strcmp(selection, "5") == 0)) {
+            instructions(fp);
+        }
+        else if ((strcmp(selection, "6") == 0)) {
+            printf("Thanks for playing!\n");
+            if (fp != NULL) {
+                fclose(fp);
+            }
+            fp = NULL;
+            exit(0);
+        }
+        else {
+            printf("That is not a valid number from 1-6, please try again\n");
+            continue;
+        }
+    }
+    if (fp != NULL) {
+        fclose(fp);
+    }
+    fp = NULL;
+}
+
+int main() {
+    int score = 0;
+    int highscore = 0;
+    FILE *fp = fopen("highscore.txt", "r");
+    instructions(fp);
+    while(1) {
+        main_menu(fp);
+    }
+    return 0;
+}
---
+++ heads_or_tails.h
@@ -0,0 +1,19 @@
+#ifndef HEADS_OR_TAILS_H
+#define HEADS_OR_TAILS_H
+#include <stdio.h>
+
+void normal_mode(FILE *fp);
+void ten_guess(FILE *fp);
+void hundred_guess(FILE *fp);
+void check_highscores(FILE *fp);
+void instructions(FILE *fp);
+void main_menu(FILE *fp);
+
+
+
+
+
+
+
+
+#endif
